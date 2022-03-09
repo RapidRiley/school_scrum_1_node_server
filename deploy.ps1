2cc830133e0274ca5533adc326f61083509b8b8a
Riley <rileywarps@gmail.com>
2022-03-09 17:16:01 +0100

    feat: Run node server in detached mode.

--- deploy.ps1
+++ deploy.ps1
@@ -7,5 +7,5 @@ docker build --build-arg SERVER_PORT=${port} -t school_scrum_1_node_server .
 
 if($?) 
 {
-    docker run --rm -p ${port}:${port} --mount "type=volume,src=cern_data,dst=/data" --name school_scrum_1_node_server -t school_scrum_1_node_server
+    docker run -d --rm -p ${port}:${port} --mount "type=volume,src=cern_data,dst=/data" --name school_scrum_1_node_server -t school_scrum_1_node_server
 }
