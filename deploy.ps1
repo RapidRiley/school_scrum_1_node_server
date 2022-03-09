61c01c7dd6a191d5e98de52048eb354d876b25d3
Riley <rileywarps@gmail.com>
2022-03-09 16:05:46 +0100

    fix: changed image name and attached shared volume.

--- deploy.ps1
+++ deploy.ps1
@@ -3,9 +3,9 @@
 # Port the container uses to host the app.
 $port = 4000
 
-docker build --build-arg SERVER_PORT=${port} -t node_server .
+docker build --build-arg SERVER_PORT=${port} -t school_scrum_1_node_server .
 
 if($?) 
 {
-    docker run --rm -p ${port}:${port} node_server
+    docker run --rm -p ${port}:${port} --mount "type=volume,src=cern_data,dst=/data" --name school_scrum_1_node_server -t school_scrum_1_node_server
 }
